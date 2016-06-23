88a29351cc7193751058532e095c9314cca6013a
Uri Kalish <uri.kalish@hpe.com>
2016-06-23 10:31:59 +0300

    animation

--- art-dash-client/js/art-dash-main-ctrl.js
+++ art-dash-client/js/art-dash-main-ctrl.js
@@ -11,17 +11,22 @@ angular.module('artDashApp').controller('artDashCtrl', function artDashCtrl($sco
   }
 
   function handleServerResponse(responseData) {
-    var i = $scope.model.imageIndex % 2;
-    $scope.model.imageSrc[i] = artDashConstants.url.artBase + '/' + responseData.imagePath;
-    $scope.model.imageVisibility[i] = true;
-    $scope.model.imageVisibility[i*-1 + 1] = false;
-    $scope.model.dataInfo = responseData.dataString;
-    $scope.model.artInfo = responseData.artworkName + ' - ' + responseData.artistName + ', ' + responseData.artworkYear;
-    $scope.model.imageIndex++;
-    scheduleGetData();
+    $scope.model.showArt = false;
+    $timeout(function() {
+      var i = $scope.model.imageIndex % 2;
+      $scope.model.imageSrc[i] = artDashConstants.url.artBase + '/' + responseData.imagePath;
+      $scope.model.imageVisibility[i] = true;
+      $scope.model.imageVisibility[i*-1 + 1] = false;
+      $scope.model.dataInfo = responseData.dataString;
+      $scope.model.artInfo = responseData.artworkName + ' - ' + responseData.artistName + ', ' + responseData.artworkYear;
+      $scope.model.showArt = true;
+      $scope.model.imageIndex++;
+      scheduleGetData();
+    }, 2000);
   }
 
   $scope.model = {
+    showArt: false,
     imageIndex: 0,
     dataInfo: '',
     artInfo: '',
