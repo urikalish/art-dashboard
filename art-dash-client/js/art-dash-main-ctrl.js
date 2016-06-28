f616e8e21b25486531956c4499938db8df67a2e2
Uri Kalish <uri.kalish@hpe.com>
2016-06-28 12:54:08 +0300

    support for missing data

--- art-dash-client/js/art-dash-main-ctrl.js
+++ art-dash-client/js/art-dash-main-ctrl.js
@@ -15,7 +15,14 @@ angular.module('artDashApp').controller('artDashCtrl', function artDashCtrl($sco
     $timeout(function() {
       $scope.model.imageSrc = artDashConstants.url.artBase + '/' + responseData.art.imagePath;
       $scope.model.artInfo = responseData.art.artistName + ' - "' + responseData.art.artworkName + '", ' + responseData.art.artworkYear;
-      $scope.model.dataInfo = responseData.general.description + ': ' + responseData.data.value + ' (' + responseData.data.percentage + '%)';
+      $scope.model.dataInfo = responseData.general.description;
+      if (responseData.data.value && responseData.data.percentage) {
+        $scope.model.dataInfo += ' ' + responseData.data.value + ' (' + responseData.data.percentage + '%)';
+      } else if (responseData.data.value) {
+        $scope.model.dataInfo += ' ' + responseData.data.value;
+      } else if (responseData.data.percentage) {
+        $scope.model.dataInfo += ' ' + responseData.data.percentage + '%';
+      }
       $scope.model.showArt = true;
       $scope.model.clientCounter++;
       scheduleGetData();
